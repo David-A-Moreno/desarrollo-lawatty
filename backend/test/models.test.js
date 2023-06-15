ad0f1fac8fb794b0172c522d225b841d60c9f9a2
atardecer01 <rivas.lina@correounivalle.edu.co>
2023-06-15 13:03:03 -0600

    pruebasCorrecion3

--- backend/test/models.test.js
+++ backend/test/models.test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import bcrypt from 'bcrypt';
 import Usuario from '../models/Usuario.js';
-/*
+
 describe('Usuario Model - Pre Save Middleware', () => {
   let usuario;
 
@@ -22,7 +22,7 @@ describe('Usuario Model - Pre Save Middleware', () => {
     const isModifiedStub = sinon.stub(usuario, 'isModified').withArgs('password').returns(false);
     const nextSpy = sinon.spy();
   
-    //await usuario.save();
+     usuario.save();
   
     expect(isModifiedStub.calledOnce).to.be.false;
     expect(nextSpy.calledOnce).to.be.false;
@@ -31,7 +31,6 @@ describe('Usuario Model - Pre Save Middleware', () => {
 
  
 });
-*/
 
 
 describe('Usuario Model', () => {
@@ -68,7 +67,7 @@ describe('Usuario Model', () => {
     });
   });
 });
-
+/*
 describe('comprobarPassword', () => {
     it('debe comparar correctamente la contraseña proporcionada con la contraseña almacenada', async () => {
       const passwordFormulario = 'password123';
@@ -82,3 +81,47 @@ describe('comprobarPassword', () => {
       expect(result).to.be.true;
     });
   });
+
+
+  
+describe('Pre Save Middleware - usuarioSchema', () => {
+  let usuario;
+
+  beforeEach(() => {
+    usuario = new Usuario({
+      nombre: 'John Doe',
+      password: 'password123',
+      email: 'johndoe@example.com',
+    });
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('debe llamar a la función "next" si la contraseña no ha sido modificada', () => {
+    const isModifiedStub = sinon.stub(usuario, 'isModified').withArgs('password').returns(false);
+    const nextSpy = sinon.spy();
+  
+     usuario.save(); // Elimina el argumento del callback
+  
+    expect(isModifiedStub.calledOnce).to.be.true;
+    expect(nextSpy.calledOnce).to.be.true;
+  });
+  
+  it('debe encriptar la contraseña si ha sido modificada', async () => {
+    const isModifiedStub = sinon.stub(usuario, 'isModified').withArgs('password').returns(true);
+    const genSaltStub = sinon.stub(bcrypt, 'genSalt').resolves('salt123');
+    const hashStub = sinon.stub(bcrypt, 'hash').resolves('hashedPassword');
+    const nextSpy = sinon.spy();
+  
+    await usuario.save(); // Elimina el argumento del callback
+  
+    expect(isModifiedStub.calledOnce).to.be.true;
+    expect(genSaltStub.calledOnce).to.be.true;
+    expect(hashStub.calledOnceWithExactly('password123', 'salt123')).to.be.true;
+    expect(usuario.password).to.equal('hashedPassword');
+    expect(nextSpy.calledOnce).to.be.true;
+  });
+  
+});*/
