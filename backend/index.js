e932a6925c17799a7163bc343c41048474a1aa38
David Moreno <davidan7845@gmail.com>
2023-06-15 18:22:39 -0500

    cambios finales

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 conectarDB();
 
-const dominiosPermitidos = ['http://localhost:3000'];
+const dominiosPermitidos = ['https://desarrollo-lawatty.vercel.app'];
 
 const corsOptions = {
     origin: function(origin, callback) {
